ba8e3b5e12607c814291fef56b5b502897752bf1
Sravani <sravanilal1986@gmail.com>
2024-03-26 07:31:44 +0530

    Session1PL7

---
+++ src/Main.java
@@ -0,0 +1,66 @@
+ class Main{
+
+     public static void main(String[] args) {
+//         System.out.println("Welcome to I.T Mastar !!");
+
+         //variables and datatypes
+         //primitive data types --> int,short,byte,long, float, boolean, char,double
+         // numbers, decimals, characters, boolean(true| false)
+         //non primitive data types -->Strings, classes
+//         int age=20;
+////         System.out.println("Age ::"+age);
+//         double temperature=95.6;
+////         System.out.println("Temperature ::"+temperature);
+//         char isMajor='S';
+////         System.out.println("Is Ram a major ? "+isMajor);
+//         boolean isEligible=true;
+////         System.out.println("Is Ram eligible for a loan :: "+isEligible);
+
+         //operators
+         //arithmetic --> +, -, /, *, % --> / , %(modulo)
+         //relational  --> <,>,<=,>= , ==, !=
+         //&&, ||, >>,<<
+
+//         int num1=10;
+//         int num2=20;
+//         int answer=num1 *num2;
+//         System.out.println("Product of "+num1 +" and "+num2 +" is :: "+answer);
+
+         // / --> quotient and %---> remainder
+
+//         System.out.println(25/2);
+//         System.out.println(25%2);
+         int no1=123;
+//         System.out.println(no1%10);
+//         no1=no1/10;
+//
+////         System.out.println(no1);
+//         System.out.println(no1%10);
+//         no1=no1/10;
+//        System.out.println(no1%10);
+
+//        while(no1>0){
+//            System.out.println(no1%10);
+//            no1=no1/10;
+//        }
+
+//         System.out.println(10>5);
+//         System.out.println(15>=15);
+//         System.out.println(20==20);
+//         System.out.println(51!=20);
+//         int no2=50;
+//         int no3=34;
+//         if(no2==no3){
+//             System.out.println("Equal");
+//         }else{
+//             System.out.println("Not Equal ");
+//         }
+
+         int age=19;
+         if(age>=18){
+             System.out.println("Major ");
+         }else{
+             System.out.println("Minor ");
+         }
+     }
+ }
